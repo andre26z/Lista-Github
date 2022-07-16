044247e3dc6e58338728e28b1380d243a2393b16
André Augusto dos Santos <andreaugustodossantos@gmail.com>
2022-07-15 21:16:18 -0300

    c

--- script.js
+++ script.js
@@ -1,57 +1,28 @@
 "use strict";
 const ul = document.querySelector("ul");
+const h2 = document.querySelector("h2");
 const botaopesquisa = document.querySelector("#botaopesquisa");
 const ordemalfabeto = document.querySelector("#ordemalfabeto");
 const ordemdata = document.querySelector("#ordemdata");
 const pesquisa = document.querySelector("#boxpesquisa");
 
-// function busca() {
-// 	ul.innerHTML = "";
-
-// 	let topico = pesquisa.value;
-
-// 	(async function () {
-// 		try {
-// 			let url = `https://api.github.com/repos/andre26z/${topico}`;
-
-// 			const req = await fetch(url);
-// 			const jsondata = await req.json();
-// 			console.log(jsondata);
-// 			for (const dados of jsondata) {
-// 				console.log(dados);
-// 			}
-// 			jsondata.forEach((item) => {
-// 				let li = document.createElement("li");
-// 				let span = document.createElement("span");
-// 				let strong = document.createElement("strong");
-
-// 				ul.appendChild(li);
-// 				li.appendChild(strong);
-// 				strong.innerHTML = `Repositório: ${item.name.toUpperCase()}`;
-// 				li.appendChild(span);
-// 				span.innerHTML = `URL: ${item.url}`;
-// 				li.appendChild(span);
-// 				span.innerHTML = `Data Criação: ${Intl.DateTimeFormat("pt-BR").format(
-// 					new Date(item.created_at)
-// 				)}`;
-// 			});
-// 		} catch (e) {
-// 			console.log("error");
-// 		}
-// 	})();
-// }
-
 async function busca() {
 	const termopesquisado = document.querySelector("#boxpesquisa").value;
 	console.log(termopesquisado);
+
 	let url = `https://api.github.com/repos/andre26z/${termopesquisado}`;
+
+	if (termopesquisado === "") {
+		h2.innerHTML = "Por favor, digite um termo para pesquisar";
+	} else {
+		h2.innerHTML = "";
+	}
 
 	fetch(url)
 		.then((response) => response.json())
 		.then((data) => {
 			ul.innerHTML = "";
 			let li = document.createElement("li");
-
 			li.innerHTML = `
         <strong>${data.name.toUpperCase()}</strong>
         <span>URL: ${data.url}</span>
@@ -63,27 +34,30 @@ async function busca() {
 		});
 }
 
-// 	fetch(url)
-// 		.then(async (res) => {
-// 			if (!res.ok) {
-// 				throw new Error(res.status);
-// 			}
+function mostrartodos() {
+	h2.innerHTML = "";
+	fetch("https://api.github.com/users/andre26z/repos")
+		.then(async (res) => {
+			if (!res.ok) {
+				throw new Error(res.status);
+			}
 
-// 			var data = await res.json();
-// 			console.log(typeof)
+			var data = await res.json();
 
-// 			data.map((item) => {
-// 				let li = document.createElement("li");
+			data.map((item) => {
+				let li = document.createElement("li");
 
-// 				li.innerHTML = `
-//         <strong>${item.name.toUpperCase()}</strong>
-//         <span>URL: ${item.url}</span>
-//         <span>Data Criação:
-//           ${Intl.DateTimeFormat("pt-BR").format(new Date(item.created_at))}
-//         </span>
-//       `;
-// 				ul.appendChild(li);
-// 			});
-// 		})
-// 		.catch((e) => console.log(e));
-// }
+				li.innerHTML = `
+        <strong>${item.name.toUpperCase()}</strong>
+        <span>URL: ${item.url}</span>
+        <span> Data Criação: 
+          ${Intl.DateTimeFormat("pt-BR").format(
+						new Date(item.created_at)
+					)} </span>
+        
+      `;
+				ul.appendChild(li);
+			});
+		})
+		.catch((e) => console.log(e));
+}
